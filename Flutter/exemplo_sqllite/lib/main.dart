1275f120d1b93fb58c29c7ea835fbb9191b6bc78
vitorssauro <vitorvtr2003@gmail.com>
2024-04-02 21:36:03 -0300

    02/04/2024

---
+++ Flutter/exemplo_sqllite/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:exemplo_sqllite/view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Exemplo Sqllite",
+      theme: ThemeData(primarySwatch: Colors.blue
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ Flutter/exemplo_sqllite/lib/model.dart
@@ -0,0 +1,18 @@
+class ContatoModel {
+  //atributos
+  int id;
+  String nome;
+  String email;
+  String telefone;
+  String endereco;
+
+  ContatoModel ({
+    required this.id,
+    required this.nome,
+    required this.email,
+    required this.telefone,
+    required this.endereco,
+  });
+  //mapeamento
+
+}
